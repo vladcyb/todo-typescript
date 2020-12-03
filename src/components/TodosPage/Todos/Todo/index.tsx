01570f988a20826de907270645ecd4e229a160d5
Vlad Tsibizov <vladtsibizov@gmail.com>
2020-12-03 15:36:35 +0300

    fix layout

--- src/components/TodosPage/Todos/Todo/index.tsx
+++ src/components/TodosPage/Todos/Todo/index.tsx
@@ -1,6 +1,6 @@
 import { FC } from 'react'
 import { TodoType } from '../../../../store/todosReducer/types'
-import { Box, Card, CircularProgress, Grid, IconButton } from '@material-ui/core'
+import { Box, Card, CircularProgress, IconButton } from '@material-ui/core'
 import './s.scss'
 import { Cancel, Done } from '@material-ui/icons'
 import classNames from 'classnames'
@@ -39,33 +39,30 @@ const Todo: FC<T> = (props) => {
   /* classes */
   const classes = classNames('Todo', {
     Todo_done: todo.done,
-    Todo_loading: getters.loading,
   })
 
   return (
     <Box className={classes} mt={1}>
       <Card className="Todo__card">
-        <Grid container spacing={6} justify="space-between" alignItems="center" wrap="nowrap">
-          <Grid item xs={11}>
-            <div className="Todo__title">{todo.title}</div>
-            <div className="Todo__description">{todo.description}</div>
-          </Grid>
-          <Grid className="Todo__mark" item xs={1}>
-            {getters.loading ? (
-              <div><CircularProgress /></div>
+        <div className="Todo__text">
+          <div className="Todo__title">{todo.title}</div>
+          <div className="Todo__description">{todo.description}</div>
+        </div>
+        <div className="Todo__mark">
+          {getters.loading ? (
+            <div><CircularProgress size="30px" /></div>
+          ) : (
+            todo.done ? (
+              <IconButton className="Todo__toggle" onClick={toggle}>
+                <Done className="Todo__doneIcon" fontSize="large" />
+              </IconButton>
             ) : (
-              todo.done ? (
-                <IconButton className="Todo__toggle" onClick={toggle}>
-                  <Done className="Todo__doneIcon" fontSize="large" />
-                </IconButton>
-              ) : (
-                <IconButton className="Todo__toggle" onClick={toggle}>
-                  <Cancel className="Todo__notDoneIcon" fontSize="large" />
-                </IconButton>
-              )
-            )}
-          </Grid>
-        </Grid>
+              <IconButton className="Todo__toggle" onClick={toggle}>
+                <Cancel className="Todo__notDoneIcon" fontSize="large" />
+              </IconButton>
+            )
+          )}
+        </div>
       </Card>
     </Box>
   )

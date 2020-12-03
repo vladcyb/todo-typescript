9f6926f5f3e6db8e451c2dbeb4706c2ae535b5a4
Vlad Tsibizov <vladtsibizov@gmail.com>
2020-12-03 19:32:34 +0300

    fix ui

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,7 +1,7 @@
 import * as axios from 'axios'
 
 const instance = axios.default.create({
-  baseURL: 'http://localhost:5000',
+  baseURL: 'http://192.168.1.45:5000',
   timeout: 100000,
   headers: {
     'Content-Type': 'application/json',
